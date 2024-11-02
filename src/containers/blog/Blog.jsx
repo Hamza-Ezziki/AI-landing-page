358374d82eb7a0d355e54ffd34d5767aba5e9c3f
Hamza React <hamzazikipro@gmail.com>
2024-11-02 06:05:04 +0100

    fix footer links

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
+import { Article } from "../../compnents";
 import "./blog.css";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            date="sep 23, 2023"
+            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imgUrl={blog02}
+            date="sep 23, 2023"
+            title="Lorem ipsum dolor, sit amet consectetur ."
+          />
+          <Article
+            imgUrl={blog03}
+            date="sep 23, 2023"
+            title="Lorem ipsum dolor, sit amet consectetur ."
+          />
+          <Article
+            imgUrl={blog04}
+            date="sep 23, 2023"
+            title="Lorem ipsum dolor, sit amet consectetur ."
+          />
+          <Article
+            imgUrl={blog05}
+            date="sep 23, 2023"
+            title="Lorem ipsum dolor, sit amet consectetur ."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import gpt3Logo from "../../assets/logo.svg";
 import "./footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi quas
+          unde debitis eaque quis quae quam error eius recusandae labore alias.
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="Logo" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Link 1</p>
+          <p>Link 2</p>
+          <p>Link 3</p>
+          <p>Link 4</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Link 5</p>
+          <p>Link 6</p>
+          <p>Link 7</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Link 8</p>
+          <p>Link 9</p>
+          <p>Link 10</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>@copy Hamza Ezziki</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
